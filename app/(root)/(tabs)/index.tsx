2d9b8118894ce5491e52d40dc5e7512bda85d1fa
Ricardo Reiter <ricardoreiterr@gmail.com>
2025-02-06 10:38:21 -0300

    first commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="my-10 text-3xl font-rubik-bold">Welcome to ReState</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,13 @@
+import { useLocalSearchParams } from "expo-router";
+import { View, Text } from "react-native";
+
+const Property = () => {
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>Property {id}</Text>
+    </View>
+  );
+};
+
+export default Property;
